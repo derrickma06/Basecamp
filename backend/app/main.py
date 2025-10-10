d872800203811dae48bfadbdde795428f56ac0f3
derrickma06 <derrickma01@gmail.com>
2025-10-09 22:16:07 -0400

    Added CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
 import os
 
@@ -7,6 +8,14 @@ app = FastAPI()
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017")
 client = MongoClient(MONGO_URI)
 db = client["mydatabase"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
